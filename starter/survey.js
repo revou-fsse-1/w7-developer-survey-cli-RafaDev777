b76149888f0243dc4432b3a2282c223a04c77797
rafael-macbook <rafaeljonathan.work@gmail.com>
2023-03-09 11:46:59 +0700

    add question

--- starter/survey.js
+++ starter/survey.js
@@ -2,6 +2,13 @@ import inquirer from "inquirer";
 
 const questions = [
   // Type your question here
+  questName = "What's your first name?",
+  questEmail = `Hello ${questName}, What's your email address?`,
+  questExpDev = "Are your experienced Developer?",
+  questYears = "How many years of experience you have with JavaScript?",
+  questJavaLib = "What JavaScript library do you know?",
+  questSalary = "What is your desired salary?"
+
 ];
 
 // run your command
